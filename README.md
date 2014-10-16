2367221dde4b0d7be95fe2984b0db065557cd909
Iskren.Stanislavov <iskren.jgd@gmail.com>
2014-10-16 15:33:26 +0300

    some functions

--- README.md
+++ README.md
@@ -2,3 +2,11 @@ turtleGFx
 =========
 
 exploring turtle graphics with javascript
+
+http://en.wikipedia.org/wiki/Hilbert_curve
+http://en.literateprograms.org/Turtle_graphics_(JavaScript)
+http://mathworld.wolfram.com/HilbertCurve.html
+http://demonstrations.wolfram.com/HilbertAndMooreFractalCurves/
+http://mathforum.org/advanced/robertd/lsys2d.html
+http://demonstrations.wolfram.com/TurtleGraphics/
+За изчертаването на кривата на Хилберт е нужно да се раздели екрана на 4 квадранта
--- turtleGFx.js
+++ turtleGFx.js
@@ -1,28 +1,40 @@
 var Pen = function(){
-	this.color = arguments['color'] || "#000000";
-	this.width = arguments['width'] || 1.0;
-	// this.up = arguments['up'] || !arguments['down'] || false;
-	// this.down = arguments['down'] || !this.up;
-	this.state = arguments["state"] || "up";
+	this.color = "black";
+	this.width = 1.0;
+	this.up = true;
+	this.down = !this.up;
 };
 
 var Turtle = function(){
 	this.pen = new Pen();
-	this.orientation
+	this.location = {
+		'x': 0,
+		'y': 0
+	}; 
+	this.orientation = 0;//degrees
+	this.canvas = document.getElementById("stage");
+	this.canvasCtx = this.canvas.getContext('2d');
+	this.applyStrokeStyle();
 };
 
-// Turtle.prototype.isPenDown = function(){
-// 	return this.pen.down;
-// };
+Turtle.prototype.applyStrokeStyle = function(degrees){
+	this.canvasCtx.lineWidth = this.pen.width;
+	this.canvasCtx.strokeStyle = this.pen.color;
+};
 
 Turtle.prototype.move = function(distance){
-	if (this.pen.state === "down"){
-		//draw line to
-		// this._draw(distance);
+	var move = this.calculateMove(distance);
+	if (this.pen.up){
+		this.canvasCtx.moveTo(move.x, move.y);
 	} else {
-		//simply move line to
-		// this._justMove(distance);
+		this.canvasCtx.lineTo(move.x, move.y);
 	}
+};
+
+Turtle.prototype.calculateMove = function(distance){
+	this.location['x'] += Math.cos(this.orientation) * distance;
+	this.location['y'] += Math.sin(this.orientation) * distance;
+	return this.location;
 };
 
 Turtle.prototype.turn = function(degrees){
